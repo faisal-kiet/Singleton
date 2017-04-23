9d02294256522377412a04fa239a5ec833135f79
faisal-kiet <faisal.kiet@yahoo.com>
2017-04-24 00:59:00 +0500

    Add files via upload

---
+++ bin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton st = Singleton.getInstance();
+            st.temp = 123;
+            st.doSomthing();
+            //Console.WriteLine(st.temp);
+
+            Singleton st2 = Singleton.getInstance();
+            st2.temp = 456;
+            //Console.WriteLine(st2.temp);
+            st2.doSomthing();
+            st.doSomthing();
+        }
+    }
+}
---
+++ bin/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Singleton
+    {
+        private static Singleton st;
+
+        private Singleton()
+        {
+            Console.WriteLine("Singleton instance instantiated...");
+        }
+
+        public static Singleton getInstance()
+        {
+            if (st==null)
+            {
+                st = new Singleton();
+            }
+
+            return st;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Single instance...");
+        }
+    }
+}
